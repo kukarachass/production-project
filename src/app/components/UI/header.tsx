e3a5c36babd584049fdb7417523c5d1d1162ea18
kukarachass <maksimvozniy18@gmail.com>
2025-12-12 22:19:37 +0100

    header added

---
+++ src/app/components/UI/header.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import {Button} from "@heroui/react";
+import cn from "classnames";
+import {usePathname} from "next/navigation";
+import {siteConfig} from "@/app/config/config";
+
+export default function HeaderNav() {
+
+    const getNavItems = () => {
+        return (
+            siteConfig.navItems.map((item) => {
+                const isActive = pathName === item.href;
+                return (
+                    <Link
+                        className={cn("cursor-pointer h-9 bg-neutral-800 relative inline-flex items-center justify-center gap-2 text-sm font-bold rounded-md px-3", item.classname, {
+                            [item.active]: isActive,
+                        })}
+                        href={item.href}
+                        key={item.href}
+                    >
+                        <Image className="self-end object-contain" src={item.image} width={35} height={35}
+                               alt={item.label}/>
+                        {item.label}
+                    </Link>
+                )
+            })
+        )
+    }
+
+    const pathName = usePathname(); // navLink
+
+    return (
+        <div className="flex items-center justify-between p-6 bg-neutral-900">
+            <div className="flex items-center gap-3">
+                <Image src="/logo2.svg" alt="logo" width={35} height={35}/>
+                <span className="font-bold text-lg">recipe.</span>
+            </div>
+            <div className="flex items-center gap-4">
+                {getNavItems()}
+            </div>
+            <div className="flex flex-wrap gap-3">
+                <Button className=""> Sign In</Button>
+                <Button className={"bg-inherit border border-gray-700 "} variant="secondary">Sign Out</Button>
+            </div>
+        </div>
+
+    )
+}
---
+++ src/app/config/config.ts
@@ -0,0 +1,25 @@
+export const siteConfig = {
+     navItems: [
+        {
+            href: "/",
+            label: "Recipes",
+            classname: "hover:text-yellow-500 transition-colors duration-200",
+            active: "text-yellow-500",
+            image: "/recipe.svg",
+        },
+        {
+            href: "/ingredients",
+            label: "Ingredients",
+            classname: "hover:text-green-500 transition-colors duration-200",
+            active: "text-green-500",
+            image: "/grocery.svg",
+        },
+        {
+            href: "/about",
+            label: "About Us",
+            classname: "hover:text-blue-500 transition-colors duration-200",
+            active: "text-blue-500",
+            image: "/about.svg"
+        },
+    ]
+}
