3c4e5708b5659bc0b4d2f0c2d07de59403012976
kukarachass <maksimvozniy18@gmail.com>
2025-12-14 18:21:13 +0100

    header icon changed

--- src/app/components/UI/header.tsx
+++ src/app/components/UI/header.tsx
@@ -2,13 +2,10 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import {Button} from "@heroui/react";
 import cn from "classnames";
 import {usePathname} from "next/navigation";
 import {siteConfig} from "@/app/config/config";
 import {layoutConfig} from "@/app/config/layout.config";
-import LoginForm from "@/app/Forms/LoginForm";
-import RegisterForm from "@/app/Forms/RegisterForm";
 import AuthForm from "@/app/Forms/AuthForm";
 
 
@@ -20,16 +17,13 @@ export default function HeaderNav() {
                 const isActive = pathName === item.href;
                 return (
                     <Link
-                        // Сохраняем items-center для центрирования текста (item.label)
                         className={cn("cursor-pointer h-9 bg-neutral-800 relative flex items-center justify-center gap-2 text-sm font-bold rounded-md px-3", item.classname, {
                             [item.active]: isActive,
                         })}
                         href={item.href}
                         key={item.href}
                     >
-                        {/* Добавляем обертку с flex items-end h-full */}
                         <div className="flex items-end h-full">
-                            {/* Убедитесь, что здесь нет self-end */}
                             <Image className="object-contain" src={item.image} width={35} height={35}
                                    alt={item.label}/>
                         </div>
@@ -45,16 +39,15 @@ export default function HeaderNav() {
     return (
         <div className={`flex items-center justify-between p-6 bg-neutral-900 h-[${layoutConfig.headerHeight}]`}>
             <div className="flex items-center gap-3">
-                <Image src="/logo2.svg" alt="logo" width={35} height={35}/>
-                <span className="font-bold text-lg">recipe.</span>
+                <Image src="/world.svg" alt="logo" width={35} height={35}/>
+                <span className="font-bold text-lg bg-gradient-to-r from-red-600 via-white to-blue-600 bg-clip-text text-transparent">
+  Netherlands recipes.
+</span>
             </div>
             <div className="flex items-center gap-4">
                 {getNavItems()}
             </div>
-            <div className="flex flex-wrap gap-3">
-                {/*<LoginForm/>*/}
-                {/*<RegisterForm/>*/}
-                {/*<Button className={"bg-inherit border border-gray-700 "} variant="secondary">Sign Out</Button>*/}
+            <div>
                 <AuthForm/>
             </div>
         </div>
